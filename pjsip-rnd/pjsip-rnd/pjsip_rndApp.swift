c394e79b15cedaca0212958b654ce7e355c15cc4
Shahwat Hasnaine <shahwathasnaine@gmail.com>
2024-06-11 18:02:05 +0600

    Initiation with pjsip librarires

---
+++ pjsip-rnd/pjsip-rnd/pjsip_rndApp.swift
@@ -0,0 +1,17 @@
+//
+//  pjsip_rndApp.swift
+//  pjsip-rnd
+//
+//  Created by Shahwat Hasnaine on 11/6/24.
+//
+
+import SwiftUI
+
+@main
+struct pjsip_rndApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
